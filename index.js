992fe1466dbd2dcb3886b90b10edeec759969db7
vaidehee patel <pvaidehee@gmail.com>
2020-07-17 04:25:32 +0530

    All tasks done

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(name, cost, category){
-    /* Code here */
-}
-
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
+function createMenuItem(name, price, category){
+    
+  return{name,price,category};
+}
+
+console.log(createMenuItem("Coffee", 4,  "Drinks"));
+console.log(createMenuItem("Pizza", 7, "Lunch"));
+console.log(createMenuItem("Chocolate Cheesecake", 12, "Dessert"));
 
 
-/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
+
+/* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else.
+Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
 Your method should accept: 
 
@@ -24,7 +30,14 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
+burger.discount = function(string){
+  if (string === "teacher" || string === "student") 
+  return this.price * 0.75;
+  if (string === "public")
+  return this.price * 0.9;
+}
 
+console.log(burger.discount("teacher"));
 
 ///////////////Reviews (MVP)///////////////////
 
@@ -41,10 +54,17 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 /* Task 3: Console.log just Julius' feedback */
 
 
+console.log(reviews[5].feedback);
+
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+reviews.push({name: "Vaidehee" , rating: 4, feedback: "Fictitious"});
+console.log(reviews);
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"*/
+
+reviews[7].feedback= "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews);
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -59,9 +79,10 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
+    const  { name, rating, feedback} = reviews[index];
+    return `${name} gave the resturant a  ${rating}, and their feedback was: "${feedback}"`;
   }
-  
+  console.log(getReviewByIndex(reviews, 0));
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
@@ -72,9 +93,16 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
+function getLastReview(reviews) {
+    const reviewsLength = reviews.length;
+    console.log(reviewsLength);
+    const lastReview = reviews[reviewsLength - 1];
+    console.log(lastReview);
+    return `${lastReview.name} gave the resturant a ${lastReview.rating}, and their feedback was: "${lastReview.feedback}"`;
+
   } 
+
+  console.log(getLastReview(reviews));
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
